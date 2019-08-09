a7b8208ac0263dcf25ae508360ba738161c3b495
Alok Ranjan <abbiranjan@gmail.com>
2019-08-09 16:56:52 +0530

    updated code till 9th aug evening.

--- public/mockjson/data.js
+++ public/mockjson/data.js
@@ -5,6 +5,8 @@
       "name": "Glister",
       "rating": 10,
       "price": 333.33,
+      "selectedItem": 0,
+      "addToCart": false,
       "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
     },
     {
@@ -13,6 +15,8 @@
       "name": "G & H Shampoo",
       "rating": 8,
       "price": 123,
+      "selectedItem": 0,
+      "addToCart": false,
       "body": "est rerum tempore vitae\nsequi sint fee ffefwefe dfefe dvdfdefdefev fefevcdc nihivoluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
     },
     {
@@ -22,6 +26,8 @@
       "rating": 9,
       "discount": 10,
       "price": 25.50,
+      "selectedItem": 0,
+      "addToCart": false,
       "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
     },
     {
@@ -31,6 +37,8 @@
       "rating": 10,
       "discount": 20,
       "price": 1000,
+      "selectedItem": 0,
+      "addToCart": false,
       "body":  "adipisci  xsws sit amet autem dasdswdw rgtrgftrg sfdf vfdfdfv assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
     }
   ]
